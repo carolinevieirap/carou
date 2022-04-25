7a2ea84664cf79b27aed109b834d8cb650c5437c
hey-romy <101740500+hey-romy@users.noreply.github.com>
2022-04-25 16:18:12 -0300

    Update PageHead.tsx

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -23,7 +23,7 @@ export const PageHead: React.FC<types.PageProps> = ({ site }) => {
 
       <meta name='theme-color' content='#879ea8' />
       <meta property='og:type' content='website' />
-      <meta property='og:image' content='https://romyvonerlea.com/social.png'/>
+      <meta property='og:image' content='https://romyvonerlea.com/social.png' />
       <meta name='twitter:card' content='summary_large_image' />
       <meta name='twitter:image' content='https://romyvonerlea.com/social.png' />
 
